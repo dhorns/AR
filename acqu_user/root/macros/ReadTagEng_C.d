5f966d61777a7023f03cf3cbae4990bb75176408
David Hornidge <dhornidge@mta.ca>
2026-03-18 09:57:53 -0300

    deleted:    acqu_user/root/macros/ReadTagEng_C.d

--- acqu_user/root/macros/ReadTagEng_C.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./root/macros/ReadTagEng_C.so: /opt/root_v6.36.00/include/RVersion.h /opt/root_v6.36.00/include/ROOT/RConfig.hxx /opt/root_v6.36.00/include/TClass.h /opt/root_v6.36.00/include/TDictAttributeMap.h /opt/root_v6.36.00/include/TInterpreter.h /opt/root_v6.36.00/include/TROOT.h /opt/root_v6.36.00/include/TBuffer.h /opt/root_v6.36.00/include/TMemberInspector.h /opt/root_v6.36.00/include/TError.h /opt/root_v6.36.00/include/RtypesImp.h /opt/root_v6.36.00/include/TIsAProxy.h /opt/root_v6.36.00/include/TFileMergeInfo.h /opt/root_v6.36.00/include/TCollectionProxyInfo.h /opt/root_v6.36.00/bin/rootcling
-ReadTagEng_C__ROOTBUILDVERSION= 6.36.000
